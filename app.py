80fef28f90c4d9fe9e0223c88e11d316a153b69a
abdulrahmanabdullah <nfs056@gmail.com>
2018-11-16 21:54:04 +0300

    create makeDirectory method

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 class Organizing:
+
+    def __init__(self):
+
+
+    def getSplitFileName(self, fname):
+        return fname.splie("_")[1]
+
+    def makeFolders(self, placesList):
+        for place in placesList :
+            if not os.path.existi(place):
+                os.mkdir(place)
+                
     pass
 
 
 if __name__ = "__main__":
     obj = Organizing()
-    
